a30ff7fe5adfd728ad090871eb13cadec0621b76
kien262004 <nkien6827@gmail.com>
2025-05-18 20:51:03 +0700

    update MILP

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-# from models.MILPS.project import *
-from models.heuristic.main import *
+from models.MILPS.project import *
+# from models.heuristic.main import *
 from models.utils.helper import *
 
-cfg = create_parameters('./instance/case1.txt', './output/case1.txt')
+cfg = create_parameters('./instance/case5.txt', './output/case5.txt')
 schedule = main(cfg)
-print_target_value(cfg, './instance/case1.txt', ofcontest=True)
-log_solution(schedule, './output/case04.txt')
-print_target_value(cfg, './output/case04.txt')
+print_target_value(cfg, './instance/case5.txt', ofcontest=True)
+log_solution(schedule, './output/milp_case05.txt')
+print_target_value(cfg, './output/milp_case05.txt')
